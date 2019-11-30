4aab17d4cdbc5da47406dfd1e50c63632ef07496
Jean Carl <JeanCarlButronMaster@gmail.com>
2019-11-29 21:52:43 -0500

    clase rutas 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,54 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ProductosComponent } from './productos/productos.component';
+import { AcercadeComponent } from './acercade/acercade.component';
+import { Pagina404Component } from './pagina404/pagina404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+
+	path: 'nosotros',
+	component: AcercadeComponent
+},
+
+
+{
+
+	path: 'productos',
+	component: ProductosComponent
+},
+{
+
+	path: 'servicios',
+	component: ServiciosComponent
+},
+
+{
+
+	path: 'acercade',
+	component: AcercadeComponent
+},
+
+{
+
+	path: '',
+	redirectTo: '/servicios',
+	pathMatch: 'full'
+},
+
+
+
+{
+
+	path: '**',
+	component: Pagina404Component
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
